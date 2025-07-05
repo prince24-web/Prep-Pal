dc247197d6ce05a6a48921974e4bf8639effd422
YoussufSalah <youssufsalahsaber@gmail.com>
2025-07-06 02:09:42 +0300

    Updated index.route.js to include the new routes (subscriptions & payments routes)

--- routes/index.route.js
+++ routes/index.route.js
@@ -7,11 +7,15 @@ import usersRoutes from "./users.route.js";
 import uploadsRoutes from "./uploads.route.js";
 import summariesRoutes from "./summaries.route.js";
 import flashcardsRoutes from "./flashcards.route.js";
+import paymentsRoutes from "./payments.route.js";
+import subscriptionsRoutes from "./subscriptions.route.js";
 
 router.use("/auth/", authRoutes);
 router.use("/user/", usersRoutes);
 router.use("/upload/", uploadsRoutes);
 router.use("/summarize/", summariesRoutes);
 router.use("/flashcards/", flashcardsRoutes);
+router.use("/payment/", paymentsRoutes);
+router.use("/subscription/", subscriptionsRoutes);
 
 export default router;
