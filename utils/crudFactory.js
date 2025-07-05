26f3edd10d9016cbfe4cab87a20721fd24fd0115
YoussufSalah <youssufsalahsaber@gmail.com>
2025-07-06 00:59:57 +0300

    Updated CRUD factory: added pagination and basic filtering in getAll() method

--- utils/crudFactory.js
+++ utils/crudFactory.js
@@ -1,9 +1,41 @@
 import supabase from "../config/supabaseClient.js";
 
 const createCrudHandlers = (table, user = {}) => ({
-    async getAll() {
+    async getAll(options) {
         let query = supabase.from(table).select("*");
-        if (user) query = query.eq("user_id", user.id);
+        if (user && user.id) query = query.eq("user_id", user.id);
+
+        const { page, pageSize, limit, offset, orderBy } = options;
+
+        if (page && pageSize) {
+            const calculatedOffset = (page - 1) * pageSize;
+            query = query.range(
+                calculatedOffset,
+                calculatedOffset + pageSize - 1
+            );
+        } else if (limit) {
+            query = query.limit(limit);
+            if (offset) {
+                query = query.range(offset, offset + limit - 1);
+            }
+        } else if (offset) {
+            query = query.range(offset, Infinity);
+        }
+
+        if (orderBy) {
+            if (Array.isArray(orderBy)) {
+                orderBy.forEach((order) => {
+                    query = query.order(order.column, {
+                        ascending: order.ascending || true,
+                    });
+                });
+            } else {
+                query = query.order(orderBy.column, {
+                    ascending: orderBy.ascending || true,
+                });
+            }
+        }
+
         const { data, error } = await query;
         if (error) throw error;
         return data;
